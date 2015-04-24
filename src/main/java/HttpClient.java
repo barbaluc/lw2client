f52ce58aa6f29f9606f04d099dc91d90bd811f02
Lucas BARBAY <lucas.barbay@gmail.com>
2015-04-24 23:35:22 +0200

    some changes

--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -1,116 +1,92 @@
-import java.util.*;
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class HttpClient
-{
+public class HttpClient {
     protected URL url;
     protected HttpURLConnection server;
 
-    /**
-     * @param szUrl: String object for the URL
-     */
-    public HttpClient(String szUrl) throws Exception
-    {
-        try
-        {
+
+    public HttpClient(String szUrl) throws Exception {
+        try {
             url = new URL(szUrl);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new Exception("Invalid URL");
         }
     }
 
-    /**
-     * @param method: String object for client method (POST, GET,...)
-     */
-    public void connect(String method) throws Exception
-    {
-        try
-        {
-            server = (HttpURLConnection)url.openConnection();
+
+    public void connect(String method) throws Exception {
+        try {
+            server = (HttpURLConnection) url.openConnection();
             server.setDoInput(true);
             server.setDoOutput(true);
             server.setRequestMethod(method);
-            server.setRequestProperty("Content-type",
-                    "application/xml");
+            server.setRequestProperty("Content-type", "application/xml");
             server.connect();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new Exception("Connection failed");
         }
     }
 
-    public void disconnect()
-    {
+    public void disconnect() {
         server.disconnect();
     }
 
-    public void displayResponse() throws Exception
-    {
+    public void displayResponse() throws Exception {
         String line;
 
-        try
-        {
+        try {
             BufferedReader s = new BufferedReader(new InputStreamReader(server.getInputStream()));
             line = s.readLine();
-            while (line != null)
-            {
+            while (line != null) {
                 System.out.println(line);
                 line = s.readLine();
             }
             s.close();
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             throw new Exception("Unable to read input stream");
         }
     }
 
-    public void post(String s) throws Exception
-    {
-        try
-        {
+    public void post(String s) throws Exception {
+        try {
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(server.getOutputStream()));
             bw.write(s, 0, s.length());
             bw.flush();
             bw.close();
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             throw new Exception("Unable to write to output stream");
         }
     }
 
-    public static void main(String argv[])
-    {
-        if (argv.length == 0)
+    public static void main(String argv[]) {
+        /*if (argv.length == 0)
         {
             System.out.println("Usage: java HttpClient url\r\n");
             System.exit(0);
-        }
+        }*/
 
-        try
-        {
-            HttpClient c = new HttpClient(argv[0]);
+        try {
+            //HttpClient c = new HttpClient(argv[0]);
+            HttpClient c = new HttpClient("http://lw2-barbaluc.rhcloud.com/resume");
             c.connect("GET");
+            System.out.println("Méthode GET :");
             c.displayResponse();
             c.disconnect();
 
-            System.out.println("--------------------------------");
+            System.out.println("Méthode POST :");
 
             c.connect("POST");
-            c.post("<cv_entry> <id>3</id> <firstname>George</firstname> <name>Leyeti</name> </cv_entry>");
+            c.post("<cv_entry> <id>4</id> <firstname>George</firstname> <name>Leyeti</name> </cv_entry>");
             c.displayResponse();
             c.disconnect();
 
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("LOL");
         }
     }
 }
